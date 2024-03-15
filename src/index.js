d8d243a60614e3cbba3f3743a9c7a03338745c21
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-03-15 13:12:39 +0530

    added rate limiter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+
 const app = express();
 
 const apiRoutes = require('./routes')
 
 const { ServerConfig, Logger } = require('./config');
 
+const limiter = rateLimit({
+    windowMs: 2 * 60 * 1000, // 2 minutes
+    max: 3, // Limit each IP to 3 requests per `window`
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(limiter)
 
 app.use('/api', apiRoutes);
 
--- src/middlewares/auth-middlewares.js
+++ src/middlewares/auth-middlewares.js
@@ -1,6 +1,6 @@
 const { StatusCodes } = require('http-status-codes');
 const { ErrorResponse } = require('../utils/common');
-const AppError = require('../utils/errors/app-error');
+const AppError = require('../utils/errors/app-errors');
 const { UserService } = require('../services');
 
 function validateAuthRequest(req, res, next) {
